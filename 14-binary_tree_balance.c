9c8a1e729eee0c2ad8c83d0d94084ed2f819dc84
Akingbade675 <akingbadebasit26@gmail.com>
2022-12-16 00:56:31 +0000

    fixed checker error by including the binary_tree_height function into files it is used

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,3 +18,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	return (0);
 }
+
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure height
+ *
+ * Return: the height of the binary tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left = 0, right = 0;
+
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((left > right) ? left : right);
+	}
+	return (-1);
+}
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,12 +8,35 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	int left, right;
+	if (tree)
+	{
+		int left, right;
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+		left = binary_tree_height(tree->left);
+		right = binary_tree_height(tree->right);
 
-	return (left == right ? 1 : 0);
+		return (left == right ? 1 : 0);
+	}
+	return (0);
+}
+
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure height
+ *
+ * Return: the height of the binary tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left = 0, right = 0;
+
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((left > right) ? left : right);
+	}
+	return (-1);
 }
